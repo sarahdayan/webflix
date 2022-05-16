f4ccc38dd7f9c8f149f7a8fb08e51d8c155ae093
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-05-16 10:17:17 +0200

    feat: change title

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Webflix",
   viewport: "width=device-width,initial-scale=1",
 });
 
